4d5dd25138dea66a77f6e7988ddc1dec49753788
nagamoto <takahiro.nagamoto@playground.live>
2018-09-06 17:15:15 +0900

    Comment tableを調整

--- db/migrate/20180906081353_create_comments.rb
+++ db/migrate/20180906081353_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.text :body
-      t.integer :status
+      t.integer :status, null: false, default: 0
       t.datetime :deleted_at
       t.references :user, foreign_key: true
       t.references :article, foreign_key: true
