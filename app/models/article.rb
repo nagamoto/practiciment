88a2bd528b9ad8d6a688a68b03ca8b8072c7ab68
nagamoto <takahiro.nagamoto@playground.live>
2018-09-06 17:52:24 +0900

    STASH

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  scope :filter_by_text, ->(text) { where('articles.title like :text or articles.body like :text', query: "%#{query}%") if text.present? }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
 
   has_many :articles
   has_many :comments
+
+  scope :filter_by_birthday, ->(date) { where(birthday: date) if date.present? }
+
+  scope :filter_by_date, lambda { |from, to|
+    if from.present? && to.present?
+      where(date: from..to)
+    elsif from.present?
+      where('date >= ?', from)
+    elsif to.present?
+      where('date <= ?', to)
+    end
+  }
+  scope :filter_by, ->(company_id:, query:, from:, to:) { filter_by_company(company_id).filter_by_query(query).filter_by_date(from, to) }
+
 end
