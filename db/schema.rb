d8f00e0726bf748e50d4976d307af862ef229170
nagamoto <takahiro.nagamoto@playground.live>
2018-09-06 17:15:35 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_075823) do
+ActiveRecord::Schema.define(version: 2018_09_06_081353) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,18 @@ ActiveRecord::Schema.define(version: 2018_09_06_075823) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "status", default: 0, null: false
+    t.datetime "deleted_at"
+    t.integer "user_id"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
